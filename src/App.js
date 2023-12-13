2f3242a4ec3c35dd4101154f6cdc6af69834297c
jay9949 <jay37192@gmail.com>
2023-12-13 18:01:12 +0530

    bg-color-done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
@@ -40,7 +39,7 @@ function App() {
 
   return (
     <div
-      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700
+      className={` mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700
     h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
     >
       <TopButtons setQuery={setQuery} />
@@ -54,7 +53,7 @@ function App() {
           <Forecast title="daily forecast" items={weather.daily} />
         </div>
       )}
-      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+      <ToastContainer autoClose={3500} theme="colored" newestOnTop={true} />
     </div>
   );
 }
