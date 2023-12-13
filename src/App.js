acd416b1768ca2417bf7ac5fd515193c2b69743a
jay9949 <jay37192@gmail.com>
2023-12-13 12:54:58 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import TimeAndLoaction from "./components/TimeAndLoaction";
 import TopButton from "./components/TopButton";
 
 const App = () => {
   return (
-    <div className=" mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButton />
-      <Inputs />
-    </div>
+    <>
+      <div className=" mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <TopButton />
+        <Inputs />
+
+        <TimeAndLoaction />
+        <TemperatureAndDetails />
+        <Forecast title="hourly forecast" />
+        <Forecast title="daily forecast" />
+      </div>
+    </>
   );
 };
 
